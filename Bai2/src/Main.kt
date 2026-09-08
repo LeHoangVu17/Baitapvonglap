a9a82175862450c665ba8cb2bb1da5d7b116051f
LeHoangVu17 <lhv171026@gmail.com>
2026-09-08 10:19:32 +0700

    bai2

---
+++ Bai2/src/Main.kt
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("Lê Hoàng Vũ")
+    println("Mã sinh viên : 2415053122250")
+    println("Bài 2 : Tính tổng các số từ 1 đến 100")
+    var tong = 0
+    for (i in 1 .. 100 )
+        tong +=i
+    print(" Tổng các số từ 1 đến 100 là : $tong")
+}
