e388d0357f8330b976b831fd8d653e27e8006a58
LeHoangVu17 <lhv171026@gmail.com>
2026-09-08 10:27:25 +0700

    bai3

---
+++ Bai3/src/Main.kt
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("Lê Hoàng Vũ")
+    println("Mã sinh viên : 2415053122250")
+    println("Bài 3 : In ra màn hình các số chẵn từ 1 đến 20")
+    var i = 0
+    while ( i <= 20){
+        i++
+        if (i % 2==0)
+            print("$i ")
+
+    }
+}
