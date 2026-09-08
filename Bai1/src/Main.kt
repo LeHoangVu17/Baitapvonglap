07a731a3b9a65a3d2b0766d8e6d8055c4f611c67
LeHoangVu17 <lhv171026@gmail.com>
2026-09-08 10:01:55 +0700

    bai1

---
+++ Bai1/src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("Lê Hoàng Vũ")
+    println("Mã sinh viên : 2415053122250")
+    println("Bài 1 : In ra màn hình các số từ 1 đến 10")
+    for (i in 1 .. 10)
+        print("$i ")
+    }
